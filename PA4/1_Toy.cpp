a084a97899bcd58269ea8549f0e233934ee307d6
Nick Zhang <zhangxy.pro@gmail.com>
2015-12-27 23:04:25 +0800

    4-1 COMPLETED

--- PA4/1_Toy.cpp
+++ PA4/1_Toy.cpp
@@ -1,7 +1,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#define MAXN 2000001
+#define MAX 40325
 using namespace std;
 const int SZ = 1 << 20;
 struct fastio{
@@ -12,44 +12,199 @@ struct fastio{
         setvbuf(stdout,outbuf,_IOFBF,SZ);
     }
 }io;
+class Node
+{
+public:
+    Node* prev;
+    Node* succ;
+    char* data;
+    int cnt;
 
-// 针对模式串P求next表
-int* build_next(char* P){
-    int lp = strlen(P);
-    int* next = new int[lp];
-    next[0] = -1;
-    int t = -1, j = 0;
-    while(j < lp - 1){
-        if (t == -1 || P[t] == P[j])
-        {
-            j ++;
-            t ++;
-            next[j] = t;
-        } else {
-            t = next[t];
-        }
+    Node(){}
+    Node(char* data,int cnt, Node* prev = NULL, Node* succ = NULL){
+        this->prev = prev;
+        this->succ = succ;
+        this->data = data;
+        this->cnt = cnt;
     }
-    return next;
+
+    Node* insertAsPrev(char* d,int cnt){
+        Node* p = new Node(d,cnt,prev,this);
+        prev->succ = p;
+        prev = p;
+        return p;
+    }
+
+    Node* insertAsSucc(char* d,int cnt){
+        Node* p = new Node(d,cnt,this,succ);
+        succ->prev = p;
+        succ = p;
+        return p;
+    }
+};
+
+class List
+{
+private:
+    Node* header;
+    Node* trailer;
+public:
+    int size;
+    List(){
+        char h[8] = "a";
+        char t[8] = "A";
+        header = new Node(h,-1);
+        trailer = new Node(t,-2);
+        header->prev = NULL;
+        header->succ = trailer;
+        trailer->prev = header;
+        trailer->succ = NULL;
+        size = 0;
+    }
+    Node* pop() {
+        return remove(first());
+    }
+
+    void push(char* e,int cnt) {insertAsFirst(e,cnt);}
+    Node* dequeue(){
+        return remove(first());
+    }
+    void enqueue(char* e,int cnt) {insertAsLast(e,cnt);}
+
+    Node* first() { return header->succ;}
+    Node* last() { return trailer->prev;}
+    bool isEmpty() { return (header->succ == trailer);}
+
+    Node* insertAsFirst(char* e,int cnt){
+        size ++;
+        return header->insertAsSucc(e,cnt);
+    }
+
+    Node* insertAsLast(char* e,int cnt){
+        size ++;
+        return trailer->insertAsPrev(e,cnt);
+    }
+
+    Node* remove(Node* p){
+        Node* e = new Node(p->data,p->cnt);
+        p->prev->succ = p->succ;
+        p->succ->prev = p->prev;
+        delete p;
+        size--;
+        return e;
+    }
+
+    Node* getNodeFromPos(int pos){
+        Node* n = header;
+        for (int i = 0; i <= pos; ++i)
+            n = n->succ;
+        return n;
+    }
+
+    Node* insertIntoList(int pos, char* e, int cnt){
+        Node* n = getNodeFromPos(pos);
+        n = n->insertAsPrev(e,cnt);
+        return n;
+    }
+};
+
+void swap(char& a,char& b){
+    char tmp = a;
+    a = b;
+    b = tmp;
 }
 
-// P:要匹配的模式串; T: 文本串
-int kmp(char* P,char* T){
-    int* next = build_next(P);
-    int i = 0, j = 0;
-    int lp = strlen(P);
-    int lt = strlen(T);
-    while(i < lp && j < lt){
-        if (i == -1 || P[i] == T[j])
+char* swap_line(char* toy){
+    char* new_toy = new char[8];
+    strcpy(new_toy,toy);
+    for (int i = 0; i < 4; ++i)
+    {
+        swap(new_toy[i],new_toy[7 - i]);
+    }
+    return new_toy;
+}
+
+char* shift_left(char* toy){
+    char* new_toy = new char[8];
+    strcpy(new_toy,toy);
+    for (int i = 0; i < 3; ++i)
+    {
+        swap(new_toy[i],new_toy[i + 1]);
+    }
+    for (int i = 7; i > 4; --i)
+    {
+        swap(new_toy[i],new_toy[i - 1]);
+    }
+    return new_toy;
+}
+
+char* rotate_reverse(char* toy){
+    char* new_toy = new char[8];
+    strcpy(new_toy,toy);
+    swap(new_toy[1],new_toy[2]);
+    swap(new_toy[5],new_toy[6]);
+    swap(new_toy[2],new_toy[6]);
+    return new_toy;
+}
+
+int getIndex(char* toy){
+    int convertInt = 1;
+    int factorial[8] = {5040,720,120,24,6,2,1,1};
+    int count_small;
+    // 第i位
+    for (int i = 0;i < 7;i++){
+        // 之前有多少位比这位小
+        count_small = 0;
+        for (int j = 0;j < i;j++){
+            if (int(toy[j] - '0') < int(toy[i] - '0'))
+                count_small += 1;
+        }
+        convertInt += factorial[i] * (int(toy[i] - '0') - 1 - count_small);
+    }
+    return convertInt - 1;
+}
+
+void bfs(int* steps){
+    List Q;
+    Node* curT;
+    char* tmp;
+    char init[9] = "12345678";
+    Q.enqueue(init,1);
+    steps[0] = 1;
+    int cnt = 0;
+    // printf("%d\n", steps[0]);
+
+    while(!Q.isEmpty()){
+        curT = Q.dequeue();
+
+        tmp = swap_line(curT->data);
+        if (!steps[getIndex(tmp)])
         {
-            i++;
-            j++;
-        } else {
-            i = next[i];
+            cnt++;
+            steps[getIndex(tmp)] = curT->cnt + 1;
+            Q.enqueue(tmp,curT->cnt + 1);
+        }
+
+        tmp = shift_left(curT->data);
+        if (!steps[getIndex(tmp)])
+        {
+            cnt++;
+            steps[getIndex(tmp)] = curT->cnt + 1;
+            Q.enqueue(tmp,curT->cnt + 1);
+        }
+
+        tmp = rotate_reverse(curT->data);
+        if (!steps[getIndex(tmp)])
+        {
+            cnt++;
+            steps[getIndex(tmp)] = curT->cnt + 1;
+            Q.enqueue(tmp,curT->cnt + 1);
         }
     }
-    delete [] next;
-    return (i - j);
+    // printf("bfs finished: %d\n", cnt);
 }
+
+
 
 int main(int argc, char const *argv[])
 {
@@ -57,31 +212,24 @@ int main(int argc, char const *argv[])
           freopen("input.txt", "r", stdin);
           // freopen("output.txt", "w", stdout);
     #endif
-    char s1[MAXN];
-    char s2[MAXN];
-    int n1,n2;
-    while(scanf("%s %s\n",s1,s2) != EOF){
-        // printf("%s %s\n",s1,s2);
-        n1 = strlen(s1);
-        n2 = strlen(s2);
-        // printf("%d %d\n", n1,n2);
-        if (n1 != n2)
+    int* steps = new int[MAX];
+    for (int i = 0; i < MAX; ++i)
+    {
+        steps[i] = 0;
+    }
+    bfs(steps);
+    int n;
+    char target[9];
+    scanf("%d", &n);
+    // printf("%d\n",n);
+    while(n--){
+        for (int i = 0; i < 8; ++i)
         {
-            printf("NO\n");
-            continue;
+            scanf("%s",&target[i]);
         }
-        // 将S2首尾拼接，然后用KMP算法匹配S1看是否成功
-        for (int i = n2; i < 2 * n2; ++i)
-        {
-            s2[i] = s2[i - n2];
-        }
-        s2[2 * n2] = '\0';
-        printf("%s %s\n",s1,s2);
-        printf("kmp:%d n2:%d\n", -kmp(s1,s2),n2);
-        if (kmp(s1,s2) * (-1) <= n2)
-            printf("YES\n");
-        else
-            printf("NO\n");
+        // printf("%s\n", target);
+        printf("%d\n", steps[getIndex(target)] - 1);
+    //     printf("%d\n", search(target));// 用bfs从正确状态倒序搜索target，返回最短路径
     }
     return 0;
 }
---
+++ PA4/toy_back.cpp
@@ -0,0 +1,147 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <bitset>
+using namespace std;
+
+struct StateNode{
+	int convertInt;
+	int step;
+	int toy[8];
+};
+
+// 待插入树的数组编号
+int insert_index;
+// 是否复原
+bool back;
+// 检验是否已插入
+# define MAXSIZE 40325
+bitset<MAXSIZE> bit;
+
+void whether_insert(StateNode* StateTree){
+	if (bit[StateTree[insert_index].convertInt])
+		return;
+	bit.set(StateTree[insert_index].convertInt);
+	insert_index = insert_index+1;
+	return;
+}
+
+int convertToInt(StateNode* StateTree){
+	int convertInt = 1;
+	int factorial[8] = {5040,720,120,24,6,2,1,1};
+	int count_small;
+	// 第i位
+	for (int i=0;i<7;i++){
+		// 之前有多少位比这位小
+		count_small = 0;
+		for (int j=0;j<i;j++){
+			if (StateTree[insert_index].toy[j]<StateTree[insert_index].toy[i])
+				count_small += 1;
+		}
+		convertInt += factorial[i] * (StateTree[insert_index].toy[i] - 1 - count_small );
+	}
+	// cout<<convertInt<<endl;
+	return convertInt;
+}
+
+StateNode* op_a(StateNode* StateTree,int check_index){
+		StateTree[insert_index].step = StateTree[check_index].step + 1;
+		for (int i = 0;i<8;i++){
+			StateTree[insert_index].toy[i] = StateTree[check_index].toy[7-i];
+		}
+		StateTree[insert_index].convertInt = convertToInt(StateTree);
+		if (StateTree[insert_index].convertInt==1){
+			printf("%d\n",StateTree[insert_index].step);
+			back = true;
+		}
+		whether_insert(StateTree);
+	return StateTree;
+}
+
+StateNode* op_b(StateNode* StateTree,int check_index){
+		StateTree[insert_index].step = StateTree[check_index].step + 1;
+		for (int i=3;i>0;i--){
+			StateTree[insert_index].toy[i] = StateTree[check_index].toy[i-1];
+		}
+		StateTree[insert_index].toy[0] = StateTree[check_index].toy[3];
+		for (int i=4;i<7;i++){
+			StateTree[insert_index].toy[i] = StateTree[check_index].toy[i+1];
+		}
+		StateTree[insert_index].toy[7] = StateTree[check_index].toy[4];
+		StateTree[insert_index].convertInt = convertToInt(StateTree);
+		if (StateTree[insert_index].convertInt==1){
+			printf("%d\n",StateTree[insert_index].step);
+			back = true;
+		}
+		whether_insert(StateTree);
+	return StateTree;
+}
+
+StateNode* op_c(StateNode* StateTree,int check_index){
+		StateTree[insert_index].step = StateTree[check_index].step + 1;
+		StateTree[insert_index].toy[6] = StateTree[check_index].toy[5];
+		StateTree[insert_index].toy[5] = StateTree[check_index].toy[2];
+		StateTree[insert_index].toy[2] = StateTree[check_index].toy[1];
+		StateTree[insert_index].toy[1] = StateTree[check_index].toy[6];
+		StateTree[insert_index].toy[0] = StateTree[check_index].toy[0];
+		StateTree[insert_index].toy[3] = StateTree[check_index].toy[3];
+		StateTree[insert_index].toy[4] = StateTree[check_index].toy[4];
+		StateTree[insert_index].toy[7] = StateTree[check_index].toy[7];
+		StateTree[insert_index].convertInt = convertToInt(StateTree);
+		if (StateTree[insert_index].convertInt==1){
+			printf("%d\n",StateTree[insert_index].step);
+			back = true;
+		}
+		whether_insert(StateTree);
+	return StateTree;
+}
+
+void init_StateTree(StateNode* StateTree){
+	back = false;
+	// 初始化
+	StateTree[0].step = 0;
+	insert_index = 0;
+	StateTree[0].convertInt = convertToInt(StateTree);
+	bit.set(StateTree[0].convertInt);
+	if (StateTree[0].convertInt == 1){
+		printf("%d\n",StateTree[0].step);
+		return;
+	}
+
+	// 待插入树的数组编号
+	insert_index = 1;
+	// 待检验的数组变化
+	int check_index = 0;
+	while(check_index!=insert_index){
+		op_a(StateTree,check_index);
+		if (back)
+			break;
+		op_b(StateTree,check_index);
+		if (back)
+			break;
+		op_c(StateTree,check_index);
+		if (back)
+			break;
+		check_index += 1;
+	}
+	if (!back)
+		printf("-1\n");
+	return;
+}
+
+int main()
+{
+	int N;
+	scanf("%d",&N);
+
+	StateNode* StateTree = new StateNode[MAXSIZE];
+
+	for (int i=0;i<N;i++){
+		for(int j=0;j<8;j++){
+			scanf("%d",&StateTree[0].toy[j]);
+		}
+		init_StateTree(StateTree);
+	}
+
+	return 0;
+}
